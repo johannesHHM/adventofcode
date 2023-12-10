c52886ad6e6989cc8b00676c0fc84ef870521d15
johannes <johannes.herman@gmail.com>
2023-12-10 23:00:47 +0100

    2023 day10

--- 2023/day10/main.c
+++ 2023/day10/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<unistd.h>
 
 typedef enum
 {
-    north,
-    south,
-    east,
-    west,
-    none
+    north = 1,
+    south = 2,
+    east = 3,
+    west = 4,
+    none = 9
 } direction;
 
 typedef struct
@@ -17,22 +18,144 @@ typedef struct
     int height;
 } grid;
 
-void printgrid(grid *g)
+void freemap(grid *g)
 {
     for (int y = 0; y < g->height; y++)
     {
+        free(g->map[y]);
+    }
+    free(g->map);
+}
+
+void scalemaze(grid *g)
+{
+    char **newmap;
+    newmap = malloc(g->height * 3 * sizeof(char *));
+    for (int i = 0; i < g->height * 3; i++)
+    {
+        newmap[i] = malloc(g->width * 3 * sizeof(char));
+    }
+    int nx, ny;
+    for (int y = 0; y < g->height; y++)
+    {
+        nx = y * 3;
         for (int x = 0; x < g->width; x++)
         {
-            printf("%c ", g->map[x][y]);
+            ny = x * 3;
+            switch (g->map[y][x])
+            {
+            case '.':
+                newmap[nx + 0][ny + 0] = '.';
+                newmap[nx + 0][ny + 1] = '.';
+                newmap[nx + 0][ny + 2] = '.';
+                newmap[nx + 1][ny + 0] = '.';
+                newmap[nx + 1][ny + 1] = '.';
+                newmap[nx + 1][ny + 2] = '.';
+                newmap[nx + 2][ny + 0] = '.';
+                newmap[nx + 2][ny + 1] = '.';
+                newmap[nx + 2][ny + 2] = '.';
+                break;
+            case '|':
+                newmap[nx + 0][ny + 0] = '.';
+                newmap[nx + 0][ny + 1] = '|';
+                newmap[nx + 0][ny + 2] = '.';
+                newmap[nx + 1][ny + 0] = '.';
+                newmap[nx + 1][ny + 1] = '|';
+                newmap[nx + 1][ny + 2] = '.';
+                newmap[nx + 2][ny + 0] = '.';
+                newmap[nx + 2][ny + 1] = '|';
+                newmap[nx + 2][ny + 2] = '.';
+                break;
+            case '-':
+                newmap[nx + 0][ny + 0] = '.';
+                newmap[nx + 0][ny + 1] = '.';
+                newmap[nx + 0][ny + 2] = '.';
+                newmap[nx + 1][ny + 0] = '-';
+                newmap[nx + 1][ny + 1] = '-';
+                newmap[nx + 1][ny + 2] = '-';
+                newmap[nx + 2][ny + 0] = '.';
+                newmap[nx + 2][ny + 1] = '.';
+                newmap[nx + 2][ny + 2] = '.';
+                break;
+
+            case 'L':
+                newmap[nx + 0][ny + 0] = '.';
+                newmap[nx + 0][ny + 1] = '|';
+                newmap[nx + 0][ny + 2] = '.';
+                newmap[nx + 1][ny + 0] = '.';
+                newmap[nx + 1][ny + 1] = 'L';
+                newmap[nx + 1][ny + 2] = '-';
+                newmap[nx + 2][ny + 0] = '.';
+                newmap[nx + 2][ny + 1] = '.';
+                newmap[nx + 2][ny + 2] = '.';
+                break;
+            case 'J':
+                newmap[nx + 0][ny + 0] = '.';
+                newmap[nx + 0][ny + 1] = '|';
+                newmap[nx + 0][ny + 2] = '.';
+                newmap[nx + 1][ny + 0] = '-';
+                newmap[nx + 1][ny + 1] = 'J';
+                newmap[nx + 1][ny + 2] = '.';
+                newmap[nx + 2][ny + 0] = '.';
+                newmap[nx + 2][ny + 1] = '.';
+                newmap[nx + 2][ny + 2] = '.';
+                break;
+            case '7':
+                newmap[nx + 0][ny + 0] = '.';
+                newmap[nx + 0][ny + 1] = '.';
+                newmap[nx + 0][ny + 2] = '.';
+                newmap[nx + 1][ny + 0] = '-';
+                newmap[nx + 1][ny + 1] = '7';
+                newmap[nx + 1][ny + 2] = '.';
+                newmap[nx + 2][ny + 0] = '.';
+                newmap[nx + 2][ny + 1] = '|';
+                newmap[nx + 2][ny + 2] = '.';
+                break;
+            case 'F':
+                newmap[nx + 0][ny + 0] = '.';
+                newmap[nx + 0][ny + 1] = '.';
+                newmap[nx + 0][ny + 2] = '.';
+                newmap[nx + 1][ny + 0] = '.';
+                newmap[nx + 1][ny + 1] = 'F';
+                newmap[nx + 1][ny + 2] = '-';
+                newmap[nx + 2][ny + 0] = '.';
+                newmap[nx + 2][ny + 1] = '|';
+                newmap[nx + 2][ny + 2] = '.';
+                break;
+            case 'S':
+                newmap[nx + 0][ny + 0] = '.';
+                newmap[nx + 0][ny + 1] = '|';
+                newmap[nx + 0][ny + 2] = '.';
+                newmap[nx + 1][ny + 0] = '-';
+                newmap[nx + 1][ny + 1] = 'S';
+                newmap[nx + 1][ny + 2] = '-';
+                newmap[nx + 2][ny + 0] = '.';
+                newmap[nx + 2][ny + 1] = '|';
+                newmap[nx + 2][ny + 2] = '.';
+                break;
+            default:
+                newmap[nx + 0][ny + 0] = ',';
+                newmap[nx + 0][ny + 1] = ',';
+                newmap[nx + 0][ny + 2] = ',';
+                newmap[nx + 1][ny + 0] = ',';
+                newmap[nx + 1][ny + 1] = ',';
+                newmap[nx + 1][ny + 2] = ',';
+                newmap[nx + 2][ny + 0] = ',';
+                newmap[nx + 2][ny + 1] = ',';
+                newmap[nx + 2][ny + 2] = ',';
+                break;
+            }
         }
-        printf("\n");
     }
+    freemap(g);
+    g->map = newmap;
+    g->width *= 3;
+    g->height *= 3;
 }
 
 int countlines(char *filepath)
 {
     FILE *file = fopen(filepath, "r");
-
     int lines = 0;
     char ch;
     while (!feof(file))
@@ -41,7 +164,6 @@ int countlines(char *filepath)
         if (ch == '\n')
             lines++;
     }
-
     fclose(file);
     return lines;
 }
@@ -49,7 +171,6 @@ int countlines(char *filepath)
 int countwidth(char *filepath)
 {
     FILE *file = fopen(filepath, "r");
-
     char ch;
     int count = -1;
     do
@@ -57,7 +178,6 @@ int countwidth(char *filepath)
         ch = fgetc(file);
         count++;
     } while (ch != '\n');
-
     fclose(file);
     return count;
 }
@@ -67,10 +187,10 @@ void readfile(FILE *file, grid *g)
     char ch;
     for (int y = 0; y < g->height; y++)
     {
-        for (int x = 0; x < g->height; x++)
+        for (int x = 0; x < g->width; x++)
         {
             ch = fgetc(file);
-            g->map[x][y] = ch;
+            g->map[y][x] = ch;
         }
         fgetc(file);
     }
@@ -79,15 +199,13 @@ void readfile(FILE *file, grid *g)
 void findstart(grid *g, int *sx, int *sy)
 {
     for (int y = 0; y < g->height; y++)
-    {
-        for (int x = 0; x < g->height; x++)
-            if (g->map[x][y] == 'S')
+        for (int x = 0; x < g->width; x++)
+            if (g->map[y][x] == 'S')
             {
                 *sx = x;
                 *sy = y;
                 return;
             }
-    }
 }
 
 direction nextpipe(direction from, char nextpipe)
@@ -101,35 +219,30 @@ direction nextpipe(direction from, char nextpipe)
         else if (from == north)
             nextdirection = south;
         break;
-
     case '-':
         if (from == west)
             nextdirection = east;
         else if (from == east)
             nextdirection = west;
         break;
-
     case 'L':
         if (from == north)
             nextdirection = east;
         else if (from == east)
             nextdirection = north;
         break;
-
     case 'J':
         if (from == north)
             nextdirection = west;
         else if (from == west)
             nextdirection = north;
         break;
-
     case '7':
         if (from == south)
             nextdirection = west;
         else if (from == west)
             nextdirection = south;
         break;
-
     case 'F':
         if (from == south)
             nextdirection = east;
@@ -187,161 +300,85 @@ char checkconnects(direction from, char pipe)
         return 1;
 }
 
-void walkmaze(grid *g)
+int walkmaze(grid *g, char replace, int scale)
 {
     int x = -1;
     int y = -1;
     findstart(g, &x, &y);
-    printf("Start at (%d, %d)\n", x, y);
 
     int steps = 0;
     direction too = none;
-    if (y > 0 && checkconnects(north, g->map[x][y - 1]))
+    if (y > scale - 1 && checkconnects(north, g->map[y - scale][x]))
         too = north;
-    if (y < g->height - 2 && checkconnects(south, g->map[x][y + 1]))
+    if (y < g->height - 2 && checkconnects(south, g->map[y + scale][x]))
         too = south;
-    if (x > 0 && checkconnects(west, g->map[x - 1][y]))
+    if (x > scale - 1 && checkconnects(west, g->map[y][x - scale]))
         too = west;
-    if (x < g->width - 2 && checkconnects(east, g->map[x + 1][y]))
+    if (x < g->width - 2 && checkconnects(east, g->map[y][x + scale]))
         too = east;
-    // too = east;
-
+    int px, py;
     do
     {
+        px = x;
+        py = y;
         walkdirection(too, &x, &y);
-        // printf("on: (%d, %d)\n", x, y);
-        too = nextpipe(opposite(too), g->map[x][y]);
-        steps++;
-        // sleep(2);
-    } while (g->map[x][y] != 'S');
 
-    printf("steps: %d\n", steps);
-    printf("furthest: %d\n", steps / 2);
+        too = nextpipe(opposite(too), g->map[y][x]);
+        if (replace && g->map[py][px] != 'S')
+            g->map[py][px] = '#';
+        steps++;
+    } while (g->map[y][x] != 'S');
+    return steps;
 }
 
-void scalemaze(grid *g)
+void flood(grid *g, char fill, int x, int y, char *inside)
 {
-    char **newmap;
-    newmap = malloc(g->height * 3 * sizeof(char *));
-    for (int i = 0; i < g->height * 3; i++)
+    if (x < 0 || y < 0 || x >= g->width || y >= g->height)
     {
-        newmap[i] = malloc(g->width * 3 * sizeof(char));
+        *inside = 0;
+        return;
     }
-
-    int nx, ny;
-    for (int y = 0; y < g->height; y++)
+    if (g->map[y][x] != '#' && g->map[y][x] != fill && g->map[y][x] != 'S')
     {
-        ny = y * 3;
-        for (int x = 0; x < g->height; x++)
+        g->map[y][x] = fill;
+        flood(g, fill, x + 1, y, inside);
+        flood(g, fill, x - 1, y, inside);
+        flood(g, fill, x, y + 1, inside);
+        flood(g, fill, x, y - 1, inside);
+    }
+}
+
+void findinside(grid *g, int *x, int *y)
+{
+    int sx = -1;
+    int sy = -1;
+    findstart(g, &sx, &sy);
+    char fill = 'a';
+    for (int i = -1; i < 2; i++)
+        for (int j = -1; j < 2; j++)
         {
-            nx = x * 3;
-            switch (g->map[x][y])
+            fill++;
+            if (i == 0 || j == 0)
+                continue;
+            char found = 1;
+            flood(g, fill, sx + i, sy + j, &found);
+            if (found)
             {
-            case '.':
-                newmap[nx][ny] = '.';
-                newmap[nx + 1][ny] = '.';
-                newmap[nx + 2][ny] = '.';
-                newmap[nx][ny + 1] = '.';
-                newmap[nx + 1][ny + 1] = '.';
-                newmap[nx + 2][ny + 1] = '.';
-                newmap[nx][ny + 2] = '.';
-                newmap[nx + 1][ny + 2] = '.';
-                newmap[nx + 2][ny + 2] = '.';
-                break;
-            case '|':
-                newmap[nx][ny] = '.';
-                newmap[nx + 1][ny] = '|';
-                newmap[nx + 2][ny] = '.';
-                newmap[nx][ny + 1] = '.';
-                newmap[nx + 1][ny + 1] = '|';
-                newmap[nx + 2][ny + 1] = '.';
-                newmap[nx][ny + 2] = '.';
-                newmap[nx + 1][ny + 2] = '|';
-                newmap[nx + 2][ny + 2] = '.';
-                break;
-            case '-':
-                newmap[nx][ny] = '.';
-                newmap[nx + 1][ny] = '.';
-                newmap[nx + 2][ny] = '.';
-                newmap[nx][ny + 1] = '-';
-                newmap[nx + 1][ny + 1] = '-';
-                newmap[nx + 2][ny + 1] = '-';
-                newmap[nx][ny + 2] = '.';
-                newmap[nx + 1][ny + 2] = '.';
-                newmap[nx + 2][ny + 2] = '.';
-                break;
-            case 'L':
-                newmap[nx][ny] = '.';
-                newmap[nx + 1][ny] = '|';
-                newmap[nx + 2][ny] = '.';
-                newmap[nx][ny + 1] = '.';
-                newmap[nx + 1][ny + 1] = 'L';
-                newmap[nx + 2][ny + 1] = '-';
-                newmap[nx][ny + 2] = '.';
-                newmap[nx + 1][ny + 2] = '.';
-                newmap[nx + 2][ny + 2] = '.';
-                break;
-            case 'J':
-                newmap[nx][ny] = '.';
-                newmap[nx + 1][ny] = '|';
-                newmap[nx + 2][ny] = '.';
-                newmap[nx][ny + 1] = '-';
-                newmap[nx + 1][ny + 1] = 'J';
-                newmap[nx + 2][ny + 1] = '.';
-                newmap[nx][ny + 2] = '.';
-                newmap[nx + 1][ny + 2] = '.';
-                newmap[nx + 2][ny + 2] = '.';
-                break;
-            case '7':
-                newmap[nx][ny] = '.';
-                newmap[nx + 1][ny] = '.';
-                newmap[nx + 2][ny] = '.';
-                newmap[nx][ny + 1] = '-';
-                newmap[nx + 1][ny + 1] = '7';
-                newmap[nx + 2][ny + 1] = '.';
-                newmap[nx][ny + 2] = '.';
-                newmap[nx + 1][ny + 2] = '|';
-                newmap[nx + 2][ny + 2] = '.';
-                break;
-            case 'F':
-                newmap[nx][ny] = '.';
-                newmap[nx + 1][ny] = '.';
-                newmap[nx + 2][ny] = '.';
-                newmap[nx][ny + 1] = '.';
-                newmap[nx + 1][ny + 1] = 'F';
-                newmap[nx + 2][ny + 1] = '-';
-                newmap[nx][ny + 2] = '.';
-                newmap[nx + 1][ny + 2] = '|';
-                newmap[nx + 2][ny + 2] = '.';
-                break;
-            case 'S':
-                newmap[nx][ny] = 'S';
-                newmap[nx + 1][ny] = 'S';
-                newmap[nx + 2][ny] = 'S';
-                newmap[nx][ny + 1] = 'S';
-                newmap[nx + 1][ny + 1] = 'S';
-                newmap[nx + 2][ny + 1] = 'S';
-                newmap[nx][ny + 2] = 'S';
-                newmap[nx + 1][ny + 2] = 'S';
-                newmap[nx + 2][ny + 2] = 'S';
-                break;
-            default:
-                newmap[nx][ny] = '.';
-                newmap[nx + 1][ny] = '.';
-                newmap[nx + 2][ny] = '.';
-                newmap[nx][ny + 1] = '.';
-                newmap[nx + 1][ny + 1] = '.';
-                newmap[nx + 2][ny + 1] = '.';
-                newmap[nx][ny + 2] = '.';
-                newmap[nx + 1][ny + 2] = '.';
-                newmap[nx + 2][ny + 2] = '.';
-                break;
+                *x = sx + i;
+                *y = sy + j;
+                return;
             }
         }
-    }
-    g->map = newmap;
-    g->width *= 3;
-    g->height *= 3;
+}
+
+long countinside(grid *g)
+{
+    long count = 0;
+    for (int y = 1; y < g->height; y += 3)
+        for (int x = 1; x < g->width; x += 3)
+            if (g->map[y][x] == 'I')
+                count++;
+    return count;
 }
 
 void parsefile(char *filepath)
@@ -357,14 +394,11 @@ void parsefile(char *filepath)
     int height, width;
     height = countlines(filepath);
     width = countwidth(filepath);
-    printf("lines: %d, width: %d\n", height, width);
 
     char **map;
     map = malloc(height * sizeof(char *));
     for (int i = 0; i < height; i++)
-    {
         map[i] = malloc(width * sizeof(char));
-    }
 
     grid grid;
     grid.map = map;
@@ -372,13 +406,26 @@ void parsefile(char *filepath)
     grid.height = height;
 
     readfile(file, &grid);
-    printgrid(&grid);
+    
+    int steps;
+    steps = walkmaze(&grid, 0, 1);
 
-    walkmaze(&grid);
+    printf("Furthest away: %d\n", steps / 2);
 
-    // scalemaze(&grid);
-    // printgrid(&grid);
+    scalemaze(&grid);
 
+    walkmaze(&grid, 1, 2);
+    
+    int sx, sy;
+    char _;
+    findinside(&grid, &sx, &sy);
+    flood(&grid, 'I', sx, sy, &_);
+    
+    long c = countinside(&grid);
+    
+    printf("Inside spots: %ld\n", c);
+
+    freemap(&grid);
     fclose(file);
 }
 
